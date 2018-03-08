8b6a8847075cb1359e3e5948719a23ae230bd1bc
Arne Heil <arne@civity.nl>
2018-03-08 11:38:34 +0100

    oke

--- app.py
+++ app.py
@@ -9,14 +9,10 @@ urls = (
 app = web.application(urls, globals())
 
 class home:
-    def GET(self, name):
-        if not name: 
-            name = 'world'
-        return 'Hello, ' + name + '!'
-    def POST(self, name):
-        if not name:
-            name = 'world'
-        return 'Hello, ' + name + '!'
+    def GET(self):
+        return 'Hello, this is a GET'
+    def POST(self):
+        return 'Hello, this is a POST'
 
 class hello:
     def GET(self, name):
