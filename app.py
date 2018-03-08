943473f9a38636555c971b65b2dedd1ebb40e891
Arne Heil <arne@civity.nl>
2018-03-08 13:14:00 +0100

    oke

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
-
 import web
 
 urls = (
-    #'/', 'home'
-    '/(.*)', 'hello'
+    '/arne', 'home'
+    # '/(.*)', 'hello'
 
 )
 app = web.application(urls, globals())
 
-# class home:
-#     def GET(self):
-#         return 'Hello, this is a GET'
-#     def POST(self):
-#         return 'Hello, this is a POST'
 
-class hello:
-    def GET(self, name):
-        if not name:
-            name = 'world'
-        return 'Hello, ' + name + '!'
-    def POST(self, name):
-        if not name:
-            name = 'world'
-        return 'Hello, ' + name + '!'
+class home:
+    def GET(self):
+        return 'Hello, this is a GET'
+
+    def POST(self):
+        return 'Hello, this is a POST'
+
+
+class person():
+    def __init__(self):
+        self.name
+    def getName(self):
+        print("asd")
 
 
 if __name__ == "__main__":
